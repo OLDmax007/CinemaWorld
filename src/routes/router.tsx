a0086435461aca5b604a492f9030e87798c8f46c
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-07-14 22:51:01 +0300

    Create a router and configure it

---
+++ src/routes/router.tsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout.tsx";
+import HomePage from "../pages/HomePage.tsx";
+import MoviesPage from "../pages/MoviesPage.tsx";
+import ErrorPage from "../pages/ErrorPage.tsx";
+
+
+export const  router = createBrowserRouter([
+    {path: '', element: <MainLayout/>, errorElement: <ErrorPage/>,children: [
+            {index: true, element: <HomePage/>},
+            {path: '/movies', element: <MoviesPage/>},
+            {path: '/movies:genre', element: <MoviesPage/>}
+        ]},
+])
