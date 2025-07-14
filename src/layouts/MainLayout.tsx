dd0578c2fc806ae1df57f0b44e542239ec277ea5
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-07-14 22:53:22 +0300

    Add Header and Footer to layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,16 @@
+import Header from "../components/Structure/Header/Header.tsx";
+import {Outlet} from "react-router-dom";
+import Footer from "../components/Structure/Footer/Footer.tsx";
+
 const MainLayout = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header/>
+            <main>
+                <Outlet/>
+            </main>
+            <Footer/>
+        </>
     );
 };
 
