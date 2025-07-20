25d3f71e84a04b892e5d865e2fa3c4dbd92d1eac
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-07-20 23:17:17 +0300

    Added console error

--- src/helpers/handleError.ts
+++ src/helpers/handleError.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 
 export const handleError = (error: unknown) => {
     if (axios.isAxiosError(error)) {
+        console.error(error.message)
         return error.message
     }
 
     if (error instanceof Error) {
+        console.warn(error.message)
         return error.message;
     }
 
