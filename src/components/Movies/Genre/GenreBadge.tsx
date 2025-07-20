ab196b0eec9129998ade6cfd75d6dde6648a2636
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-07-21 00:36:48 +0300

    Update GenreBadge Component

--- src/components/Movies/Genre/GenreBadge.tsx
+++ src/components/Movies/Genre/GenreBadge.tsx
@@ -1,22 +1,22 @@
 import type {FC} from "react";
 import type {GenreType} from "../../../models/GenreType.ts";
-import {getGenreIcon} from "../../../helpers/getGenreIcon.tsx";
-import {genreBadgeStyles} from "./styles/genreBadge.styles.ts";
-import {genreNameStyles} from "./styles/genreName.styles.ts";
 import {genreIconStyles} from "./styles/genreIcon.styles.ts";
 import type {GenresProps} from "./Genres.tsx";
 import {Link} from "react-router-dom";
+import {genreBadgeStyles} from "./styles/genreBadge.styles.ts";
+import {genreNameStyles} from "./styles/genreName.styles.ts";
+import {GenreIcon} from "./GenreIcon.tsx";
 
-type GenreBadgeProps = Pick<GenresProps, 'variant' | 'handleClick'> & {
+export type GenreBadgeProps = Pick<GenresProps, 'variant' | 'handleClick'> & {
     genre: GenreType;
 };
 
 const GenreBadge: FC<GenreBadgeProps> = ({genre, variant, handleClick}) => {
-    const Icon = getGenreIcon(genre)
+
     const content =
         <div className={genreBadgeStyles({variant})}>
             <span className={genreNameStyles({variant})}>{genre.name}</span>
-            <span><Icon className={genreIconStyles({variant})}/></span>
+            <span><GenreIcon className={genreIconStyles({variant})} genre={genre}/></span>
         </div>
 
 
