1086841d3ed380ff6d3af6dbda1c58cdb187196a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-09 23:59:06 +0300

    Change path in link

--- src/components/Movies/Genre/GenreBadge.tsx
+++ src/components/Movies/Genre/GenreBadge.tsx
@@ -1,11 +1,11 @@
-import type {FC} from "react";
+import {type FC} from "react";
 import type {GenreType} from "../../../models/GenreType.ts";
 import {genreIconStyles} from "./styles/genreIcon.styles.ts";
 import type {GenresProps} from "./Genres.tsx";
 import {Link} from "react-router-dom";
 import {genreBadgeStyles} from "./styles/genreBadge.styles.ts";
 import {genreNameStyles} from "./styles/genreName.styles.ts";
-import {GenreIcon} from "./GenreIcon.tsx";
+import GenreIcon from "./GenreIcon.tsx";
 
 export type GenreBadgeProps = Pick<GenresProps, 'variant' | 'handleClick'> & {
     genre: GenreType;
@@ -19,11 +19,10 @@ const GenreBadge: FC<GenreBadgeProps> = ({genre, variant, handleClick}) => {
             <span><GenreIcon className={genreIconStyles({variant})} genre={genre}/></span>
         </div>
 
-
     return (
         <>
             {handleClick ? (
-                <Link to={`genres/${genre.name}`} onClick={() => handleClick?.(genre)}>
+                <Link to={`/movies/${genre.name}`} onClick={() => handleClick?.(genre.id)}>
                     {content}
                 </Link>
             ) : (
