84ef22e21c1ffd24eb311b4127e2dc39256e4f67
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-09 23:58:37 +0300

    Implement basic logic, callback, handleGenreClick and condition for render components in MoviesPage

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,7 +1,38 @@
+import MoviesList from "../components/Movies/MoviesList/MoviesList.tsx";
+import Genres from "../components/Movies/Genre/Genres.tsx";
+import {useAppThunkData} from "../hooks/useAppThunkData.ts";
+import {movieThunks} from "../redux/slices/movieSlice/movieThunks.ts";
+import {useAppPageParam} from "../hooks/useAppPageParam.ts";
+import {useCallback, useState} from "react";
+import type {MovieType} from "../models/MovieType.ts";
+
 const MoviesPage = () => {
+    const [genreId, setGenreId] = useState<number | null>(null);
+    const genres = useAppThunkData(movieThunks.loadGenres, (state) => state.genres)
+    const page = useAppPageParam()
+
+    const loadMoviesCallback = useCallback(() => {
+        return movieThunks.loadMovies({
+            page,
+            ...(genreId !== null && { with_genres: genreId.toString() })
+        });
+    }, [page, genreId]);
+
+    const movies = useAppThunkData<MovieType[]>(loadMoviesCallback , (state) => state.movies)
+
+    const handleGenreClick = (genreId: number) => {
+        setGenreId(genreId)
+    };
+
     return (
-        <div>
-        </div>
+        <>
+            {(genres && movies) &&
+                <>
+                    <MoviesList movies={movies} page={page}/>
+                    <Genres genres={genres} variant={'filter'} handleClick={handleGenreClick}/>
+                </>
+            }
+        </>
     );
 };
 
