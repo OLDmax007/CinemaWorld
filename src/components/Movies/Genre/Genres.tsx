b441740e1f7f4fa86074ae7b14e2a684d3f483f9
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-09 23:53:12 +0300

    Change prop's arguments and add key in Genres

--- src/components/Movies/Genre/Genres.tsx
+++ src/components/Movies/Genre/Genres.tsx
@@ -5,14 +5,14 @@ import GenreBadge from "./GenreBadge.tsx";
 export type GenresProps = {
     genres: GenreType[]
     variant: 'default' | 'filter'
-    handleClick?: (genre?: GenreType) => void
+    handleClick?: (genreId: number) => void;
 }
 
 const Genres:FC<GenresProps> = ({genres,variant, handleClick}) => {
     return (
-        <div >
-            {genres.map((genre) =>
-                <GenreBadge variant={variant} genre={genre} handleClick={handleClick ? () => handleClick(genre) : undefined}/>
+        <div>
+            {genres.map((genre, index) =>
+                <GenreBadge key={index} variant={variant} genre={genre}  handleClick={handleClick ? () => handleClick(genre.id) : undefined}/>
             )}
         </div>
     );
