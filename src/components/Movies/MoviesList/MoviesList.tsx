9c257ffb1645502fa8ed2733d5cd063cb3ba2dea
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-09 23:55:01 +0300

    Implement basic logic, search, and page numbering in MovieList

--- src/components/Movies/MoviesList/MoviesList.tsx
+++ src/components/Movies/MoviesList/MoviesList.tsx
@@ -1,8 +1,27 @@
-const MoviesList = () => {
+import {useAppSelector} from "../../../hooks/useAppSelector.ts";
+import Pagination from "../../UI/Pagination/Pagination.tsx";
+import type {MovieType} from "../../../models/MovieType.ts";
+import {type FC} from "react";
+import MovieSearchForm from "../../UI/MovieSearchForm.tsx";
+
+type MovieListProps = {
+    movies: MovieType[]
+    page: string
+}
+
+
+const MoviesList:FC<MovieListProps> = ({movies, page}) => {
+    const queryMeta = useAppSelector(state => state.queryMeta)
     return (
-        <div>
-            
-        </div>
+        <main>
+            <ul>
+                {movies.map((movie, index) => <li key={index}>
+                    {movie.title}
+                </li>  )}
+            </ul>
+            <MovieSearchForm page={page}/>
+            <Pagination queryMeta={queryMeta} maxPages={500}/>
+        </main>
     );
 };
 
