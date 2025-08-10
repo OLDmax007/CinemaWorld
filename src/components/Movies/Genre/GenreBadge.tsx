23668622eb9efd688fe12222a5d2aa547affcc2e
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-10 23:50:48 +0300

    Remove handler and change link path

--- src/components/Movies/Genre/GenreBadge.tsx
+++ src/components/Movies/Genre/GenreBadge.tsx
@@ -2,27 +2,27 @@ import {type FC} from "react";
 import type {GenreType} from "../../../models/GenreType.ts";
 import {genreIconStyles} from "./styles/genreIcon.styles.ts";
 import type {GenresProps} from "./Genres.tsx";
-import {Link} from "react-router-dom";
 import {genreBadgeStyles} from "./styles/genreBadge.styles.ts";
 import {genreNameStyles} from "./styles/genreName.styles.ts";
 import GenreIcon from "./GenreIcon.tsx";
+import {Link} from "react-router-dom";
 
-export type GenreBadgeProps = Pick<GenresProps, 'variant' | 'handleClick'> & {
+export type GenreBadgeProps = Pick<GenresProps, 'variant'> & {
     genre: GenreType;
 };
 
-const GenreBadge: FC<GenreBadgeProps> = ({genre, variant, handleClick}) => {
+const GenreBadge: FC<GenreBadgeProps> = ({genre, variant}) => {
 
     const content =
         <div className={genreBadgeStyles({variant})}>
             <span className={genreNameStyles({variant})}>{genre.name}</span>
             <span><GenreIcon className={genreIconStyles({variant})} genre={genre}/></span>
         </div>
-
     return (
         <>
-            {handleClick ? (
-                <Link to={`/movies/${genre.name}`} onClick={() => handleClick?.(genre.id)}>
+
+            {variant === 'filter' ? (
+                <Link to={genre.id === 0 ? '/movies' : `/${genre.name}/${genre.id}`}>
                     {content}
                 </Link>
             ) : (
