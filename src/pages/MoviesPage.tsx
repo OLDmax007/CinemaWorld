74b311ef1ebdc3d25d8e3f0d02c7f60687106b64
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-10 23:57:45 +0300

    Remove handler, genreId state and change genreId validation

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -3,33 +3,30 @@ import Genres from "../components/Movies/Genre/Genres.tsx";
 import {useAppThunkData} from "../hooks/useAppThunkData.ts";
 import {movieThunks} from "../redux/slices/movieSlice/movieThunks.ts";
 import {useAppPageParam} from "../hooks/useAppPageParam.ts";
-import {useCallback, useState} from "react";
+import {useCallback} from "react";
 import type {MovieType} from "../models/MovieType.ts";
+import {useParams} from "react-router-dom";
 
 const MoviesPage = () => {
-    const [genreId, setGenreId] = useState<number | null>(null);
     const genres = useAppThunkData(movieThunks.loadGenres, (state) => state.genres)
     const page = useAppPageParam()
+    const {genreId} = useParams<{genreId: string}>()
 
     const loadMoviesCallback = useCallback(() => {
         return movieThunks.loadMovies({
             page,
-            ...(genreId !== null && { with_genres: genreId.toString() })
+            ...(genreId ? { with_genres: genreId.toString() } : {})
         });
     }, [page, genreId]);
 
     const movies = useAppThunkData<MovieType[]>(loadMoviesCallback , (state) => state.movies)
-
-    const handleGenreClick = (genreId: number) => {
-        setGenreId(genreId)
-    };
 
     return (
         <>
             {(genres && movies) &&
                 <>
                     <MoviesList movies={movies} page={page}/>
-                    <Genres genres={genres} variant={'filter'} handleClick={handleGenreClick}/>
+                    <Genres genres={genres} variant={'filter'}/>
                 </>
             }
         </>
