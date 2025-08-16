b8c981409aa84361528c50bc96c5ce6340b99c07
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-16 23:36:49 +0300

    Add module styles in pagination component

--- src/components/UI/Pagination/Pagination.tsx
+++ src/components/UI/Pagination/Pagination.tsx
@@ -1,37 +1,55 @@
 import type {FC} from "react";
 import {useSearchParams} from "react-router-dom";
 import type {TMDBResQueryType} from "../../../models/TMDBResponseType.ts";
+import styles from "./pagination.module.css";
 
 type PaginationProps = {
     queryMeta: TMDBResQueryType | null
-    maxPages: number
 }
 
-const Pagination: FC<PaginationProps> = ({queryMeta, maxPages}) => {
+const Pagination: FC<PaginationProps> = ({queryMeta}) => {
     const [searchParams, setSearchParams] = useSearchParams();
     const currentPage = Number(searchParams.get('page')) || queryMeta?.page || 1;
+    const totalPages =  queryMeta && queryMeta?.total_pages < 500 ?  queryMeta?.total_pages : 500
 
     const handlePageChange = (page: number) => {
-        if (page < 1 || page > maxPages) return
+        if (page < 1 || page > totalPages) return
         searchParams.set('page', page.toString());
         setSearchParams(searchParams);
     };
 
     return (
-        <div>
-            <button onClick={() => handlePageChange(1)}>First page</button>
-            <button disabled={currentPage <= 1} onClick={() => handlePageChange(currentPage - 1)}>
+        <nav className={styles.pagination}>
+            <button
+                className={styles.pageButton}
+                disabled={currentPage === 1}
+                onClick={() => handlePageChange(1)}
+            >
+                First
+            </button>
+            <button
+                className={styles.pageButton}
+                disabled={currentPage === 1}
+                onClick={() => handlePageChange(currentPage - 1)}
+            >
                 &lt;
             </button>
-            <span>{currentPage} of {maxPages}</span>
-            <button></button>
-            <button disabled={currentPage >= maxPages} onClick={() => handlePageChange(currentPage + 1)}>
+            <span className={styles.currentPage}>{currentPage} / {totalPages}</span>
+            <button
+                className={styles.pageButton}
+                disabled={currentPage === totalPages}
+                onClick={() => handlePageChange(currentPage + 1)}
+            >
                 &gt;
             </button>
-            <button onClick={() => handlePageChange(maxPages)}>
-                Last page
+            <button
+                className={styles.pageButton}
+                disabled={currentPage === totalPages}
+                onClick={() => handlePageChange(totalPages)}
+            >
+                Last
             </button>
-        </div>
+        </nav>
     );
 };
 
