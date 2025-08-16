36886ae44001c9ad722d95c9fe4c4ee44012f0e5
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-16 23:09:24 +0300

    Implement Poster component

--- src/components/Movies/Poster/Poster.tsx
+++ src/components/Movies/Poster/Poster.tsx
@@ -1,7 +1,22 @@
-const Poster = () => {
+
+import type {FC} from "react";
+import noImage from '../../../assets/images/no-image.png';
+import {urls} from "../../../constants/urls.ts";
+import styles from './poster.module.css'
+
+
+type PosterProps = {
+    posterPath: string
+
+}
+
+const Poster: FC<PosterProps> = ({posterPath}) => {
     return (
-        <div>
-            
+        <div className={styles.posterWrapper}>
+            <img
+                className={styles.posterImage}
+                src={posterPath ? urls.movies.posterUrl + posterPath : noImage}
+                alt="Poster"/>
         </div>
     );
 };
