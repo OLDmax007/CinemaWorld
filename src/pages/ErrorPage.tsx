4750643b8bffb6937d668686a2b2f39e2f12d438
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-17 20:24:38 +0300

    Change markup and render ErrorBlock

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,20 +1,10 @@
-import {Link, useRouteError} from "react-router-dom";
-import {handleError} from "../helpers/handleError.ts";
+import ErrorBlock from "../components/UI/ErrorBlock.tsx";
 
 const ErrorPage = () => {
-    const error: unknown = useRouteError();
-    const errorMessage = handleError(error);
     return (
-        <div className="flex flex-col items-center justify-center h-screen text-center bg-gray-100 text-gray-800">
-            <h1 className="text-3xl font-bold mb-2">Something went wrong</h1>
-            <p className="mb-4 text-red-500 italic"><i>{errorMessage}</i></p>
-            <Link
-                to="/"
-                className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-            >
-                Go back Home
-            </Link>
-        </div>
+        <section>
+        <ErrorBlock/>
+        </section>
     );
 };
 
