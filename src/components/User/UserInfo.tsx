ca84f035870da7d85531d601f1b6a9611c6cc72e
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-17 23:51:47 +0300

    Implement UserInfo component

--- src/components/User/UserInfo.tsx
+++ src/components/User/UserInfo.tsx
@@ -1,7 +1,16 @@
+import UserImg from '../../assets/images/user.png'
+
+
 const UserInfo = () => {
     return (
-        <div>
-            
+        <div className={'user-info'}>
+            <img
+                src={UserImg}
+                alt={'User avatar'}/>
+            <div>
+                <span>Maxsim Dobrovolskyi</span>
+                <p>Developer</p>
+            </div>
         </div>
     );
 };
