07d38b48c90a60d2e5fed4104f54175ec01f9db7
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-23 01:09:04 +0300

    Change cva styles and delete unnecessary cva file

--- src/components/Movies/Genre/styles/genreIcon.styles.ts
+++ src/components/Movies/Genre/styles/genreIcon.styles.ts
@@ -1,20 +1,15 @@
 import {cva} from "class-variance-authority";
 
 export const genreIconStyles = cva(
-    'genreIcon', {
+    '', {
         variants: {
-            variant: {
-                default: ['text-rgb(248, 250, 252)'],
-                filter: ['text-rgb(225, 29, 72)']
-            },
             size: {
-                small: ['w-30 h-20'],
-                medium: ['w-24 h-50'],
+                small: ['w-5 h-5'],
+                medium: ['w-8 h-8'],
             },
         },
         defaultVariants: {
-            variant: 'default',
-            size: 'small'
+            size: 'medium'
         }
     },
 )
