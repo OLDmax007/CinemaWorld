d5f26a9284f4dc368be7cbbe7d883487a2d9d428
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-22 23:58:48 +0300

    Add new routes and layout

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,12 +2,22 @@ import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout.tsx";
 import MoviesPage from "../pages/MoviesPage.tsx";
 import ErrorPage from "../pages/ErrorPage.tsx";
+import MoviePage from "../pages/MoviePage.tsx";
+import MovieSearchPage from "../pages/MovieSearchPage.tsx";
+import FiltersLayout from "../layouts/FiltersLayout.tsx";
 
 
 export const router = createBrowserRouter([
     {
-        path: '', element: <MainLayout/>, errorElement: <ErrorPage/>, children: [
-            {index: true, element: <MoviesPage/>},
-            {path: ':genreName/:genreId', element: <MoviesPage /> }
+        path: '/', element: <MainLayout/>, errorElement: <ErrorPage/>, children: [
+            {
+                path: '', element: <FiltersLayout/>, children: [
+                    {index: true, element: <MoviesPage/>},
+                    {path: '/genre/:genreName/:genreId', element: <MoviesPage/>},
+                    {path: '/movie/:movieName/:movieId', element: <MoviePage/>},
+                    {path: '/search-movies', element: <MovieSearchPage/>}
+                ]
+            },
         ]
     }])
+
