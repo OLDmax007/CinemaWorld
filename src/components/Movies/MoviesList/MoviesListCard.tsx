bfe8352b84a59a1531f5179aec89f5752f12eca5
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-23 23:33:23 +0300

    Implement MovieListCard component

--- src/components/Movies/MoviesList/MoviesListCard.tsx
+++ src/components/Movies/MoviesList/MoviesListCard.tsx
@@ -1,8 +1,35 @@
-const MoviesListCard = () => {
+import type {FC} from "react";
+import type {MovieType} from "../../../models/MovieType.ts";
+import StarsRating from "../Rating/StarsRating.tsx";
+import Poster from "../Poster/Poster.tsx";
+import StarIcon from '../Rating/star.svg?react'
+import {Link} from "react-router-dom";
+
+type MoviesListCardProps = {
+    movie: MovieType
+}
+
+
+const MoviesListCard: FC<MoviesListCardProps> = ({movie}) => {
+    const date = new Date(movie.release_date)
+
     return (
-        <div>
-            
-        </div>
+        <li>
+            <Link to={`/movie/${movie.title}/${movie.id}`}>
+            <Poster posterPath={movie.poster_path}/>
+            </Link>
+            <div>
+            <Link to={`/movie/${movie.title}/${movie.id}`}>
+                <h3>{movie.title}</h3>
+            </Link>
+            <span>{date ? date.getFullYear() : 0}</span>
+            <div className={'flex gap-5 items-center  whitespace-nowrap'}>
+                <StarsRating icon={StarIcon} count={10}
+                             defaultRating={Math.round(movie.vote_average)}/>
+                    <span className="text-sm text-yellow-500">{movie.vote_average}/{movie.vote_count} votes</span>
+            </div>
+            </div>
+        </li>
     );
 };
 
