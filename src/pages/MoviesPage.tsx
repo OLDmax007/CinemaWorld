ab54608f095b8035a411d92c70bc3ce1a7192760
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-23 23:30:33 +0300

    Move movie logic to MoviesList component

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,36 +1,10 @@
 import MoviesList from "../components/Movies/MoviesList/MoviesList.tsx";
-import Genres from "../components/Movies/Genre/Genres.tsx";
-import {useAppThunkData} from "../hooks/useAppThunkData.ts";
-import {movieThunks} from "../redux/slices/movieSlice/movieThunks.ts";
-import {useAppPageParam} from "../hooks/useAppPageParam.ts";
-import {useCallback} from "react";
-import type {MovieType} from "../models/MovieType.ts";
-import {useParams} from "react-router-dom";
 
 const MoviesPage = () => {
-    const genres = useAppThunkData(movieThunks.loadGenres, (state) => state.genres)
-    const page = useAppPageParam()
-    const {genreId} = useParams<{genreId: string}>()
-
-    const loadMoviesCallback = useCallback(() => {
-        return movieThunks.loadMovies({
-            page,
-            ...(genreId ? { with_genres: genreId.toString() } : {})
-        });
-    }, [page, genreId]);
-
-    const movies = useAppThunkData<MovieType[]>(loadMoviesCallback , (state) => state.movies)
-
     return (
         <>
-            {(genres && movies) &&
-                <>
-                    <MoviesList movies={movies} page={page}/>
-                    <Genres genres={genres} variant={'filter'}/>
-                </>
-            }
+            <MoviesList/>
         </>
     );
 };
-
 export default MoviesPage;
