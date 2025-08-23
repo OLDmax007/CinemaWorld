fcf5c547f11e1db605581f67e2e70eee73358b6d
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-23 23:34:55 +0300

    Implement MoviePage

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,9 +1,17 @@
+import MovieInfo from "../components/Movies/Movie/MovieInfo.tsx";
+import styles from '../components/Movies/Movie/movie-info.module.css'
+import {Navigate, useParams} from "react-router-dom";
+
 const MoviePage = () => {
+    const {movieId, movieName} = useParams<{ movieId: string, movieName: string }>()
+    if (!movieId || !movieName) return <Navigate to={'/404'} replace/>
+
     return (
-        <div>
-            
-        </div>
+        <section className={styles.movieInfoSection}>
+            <MovieInfo movieId={movieId} movieName={movieName}/>
+        </section>
     );
 };
+
 
 export default MoviePage;
