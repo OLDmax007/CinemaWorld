21b5dcff5182e69d86ef47e926f8406c57ee070c
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-24 23:52:23 +0300

    Create and implement LoadingWrapper component

---
+++ src/components/UI/LoadingWrapper.tsx
@@ -0,0 +1,11 @@
+import {Atom} from "react-loading-indicators";
+
+const LoadingWrapper = () => {
+        return (
+            <section className="flex justify-center items-center min-h-screen">
+                <Atom color="#dfdccc" size="large" text="" textColor="#ff00f8"/>
+            </section>
+        );
+};
+
+export default LoadingWrapper;
