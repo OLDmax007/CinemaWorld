497b97bd484ed7b986a8d680f2e45b82aeaabc6e
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-24 23:58:20 +0300

    Add apiThunk helper to fetch data

--- src/redux/slices/movieSlice/movieThunks.ts
+++ src/redux/slices/movieSlice/movieThunks.ts
@@ -1,46 +1,23 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import {apiService} from "../../../services/api.service.ts";
 import type {MovieType} from "../../../models/MovieType.ts";
 import {urlBuilder} from "../../../helpers/urlBuilder.ts";
 import {urls} from "../../../constants/urls.ts";
-import {handleError} from "../../../helpers/handleError.ts";
 import type {GenreType} from "../../../models/GenreType.ts";
 import type {TMDBResDataType, TMDBResQueryType} from "../../../models/TMDBResponseType.ts";
 import type {QueryParamsType} from "../../../models/QueryParamsType.ts";
-
+import {apiThunk} from "../../../helpers/apiThunk.ts";
 
 export const movieThunks = {
     loadMovies: createAsyncThunk('movieSlice/loadMovies', async (queryParams: QueryParamsType, thunkAPI) => {
-        try {
-            return await apiService.get<TMDBResDataType & TMDBResQueryType>(urlBuilder(urls.movies.all, {...queryParams}))
-
-        } catch (error) {
-            return thunkAPI.rejectWithValue(handleError(error))
-        }
+        return await apiThunk<TMDBResDataType & TMDBResQueryType>(thunkAPI, urlBuilder(urls.movies.all, {...queryParams}))
     }),
-
     loadMovieById: createAsyncThunk('movieSlice/loadMovie', async (id: string, thunkAPI) => {
-        try {
-            return await apiService.get<MovieType>(urlBuilder(urls.movies.getById(id)))
-        } catch (error) {
-            return thunkAPI.rejectWithValue(handleError(error))
-        }
+        return await apiThunk<MovieType>(thunkAPI, urlBuilder(urls.movies.getById(id)))
     }),
-
     loadMoviesByQuery: createAsyncThunk('movieSlice/loadMoviesByQuery', async (queryParams: QueryParamsType, thunkAPI) => {
-        try {
-            return await apiService.get<TMDBResDataType & TMDBResQueryType>(urlBuilder(urls.movies.search, {...queryParams}))
-
-        } catch (error) {
-            return thunkAPI.rejectWithValue(handleError(error))
-        }
+        return await apiThunk<TMDBResDataType & TMDBResQueryType>(thunkAPI, urlBuilder(urls.movies.search, {...queryParams}))
     }),
-
     loadGenres: createAsyncThunk('movieSlice/loadGenres', async (_, thunkAPI) => {
-        try {
-            return await apiService.get<{ genres: GenreType[] }>(urlBuilder(urls.movies.genres.all))
-        } catch (error) {
-            return thunkAPI.rejectWithValue(handleError(error))
-        }
+        return await apiThunk<{ genres: GenreType[] }>(thunkAPI, urlBuilder(urls.movies.genres.all))
     })
 }
