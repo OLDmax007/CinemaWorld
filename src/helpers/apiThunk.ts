5d1f969ec782d1496abaa0f6f39c4342e74f9a9f
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-08-24 23:55:29 +0300

    Create and implement apiThunk helper

---
+++ src/helpers/apiThunk.ts
@@ -0,0 +1,11 @@
+import {apiService} from "../services/api.service.ts";
+import {handleError} from "./handleError.ts";
+import type {GetThunkAPI} from "@reduxjs/toolkit";
+
+export const apiThunk = async <T>(thunkAPI: GetThunkAPI<{ rejectValue: string }>, url: string) => {
+    try {
+        return await apiService.get<T>(url)
+    } catch (error) {
+        return thunkAPI.rejectWithValue(handleError(error))
+    }
+}
