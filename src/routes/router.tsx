b1f8ad6114343c06dcd1c3522a8db810c1baa70a
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 01:33:54 +0300

    Update imports, change search path

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,10 @@
 import {createBrowserRouter} from "react-router-dom";
-import MainLayout from "../layouts/MainLayout.tsx";
-import MoviesPage from "../pages/MoviesPage.tsx";
-import ErrorPage from "../pages/ErrorPage.tsx";
-import MoviePage from "../pages/MoviePage.tsx";
-import MovieSearchPage from "../pages/MovieSearchPage.tsx";
-import FiltersLayout from "../layouts/FiltersLayout.tsx";
+import MainLayout from "@layouts/MainLayout.tsx";
+import MoviesPage from "@pages/MoviesPage.tsx";
+import ErrorPage from "@pages/ErrorPage.tsx";
+import MoviePage from "@pages/MoviePage.tsx";
+import MovieSearchPage from "@pages/MovieSearchPage.tsx";
+import FiltersLayout from "@layouts/FiltersLayout.tsx";
 
 
 export const router = createBrowserRouter([
@@ -15,7 +15,7 @@ export const router = createBrowserRouter([
                     {index: true, element: <MoviesPage/>},
                     {path: '/genre/:genreName/:genreId', element: <MoviesPage/>},
                     {path: '/movie/:movieName/:movieId', element: <MoviePage/>},
-                    {path: '/search-movies', element: <MovieSearchPage/>}
+                    {path: '/search', element: <MovieSearchPage/>}
                 ]
             },
         ]
