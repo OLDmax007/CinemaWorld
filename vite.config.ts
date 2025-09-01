c1317cc1dd29124976ac860962e520a547651773
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-01 23:38:29 +0300

    1. Install svg libs and configure 2. Add aliases and paths to tsconfig.app and vite config files.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,33 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite'
+import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 
 export default defineConfig({
     plugins: [
         react(),
         svgr(),
-        tailwindcss()
+        tailwindcss(),
+        createSvgSpritePlugin({
+            exportType: 'vanilla',
+            include: 'src/assets/icons/*.svg',
+            symbolId: '[name]',
+        }),
+
+
     ],
+    resolve: {
+        alias: {
+            '@': '/src',
+            '@components': '/src/components',
+            '@assets': '/src/assets',
+            '@helpers': '/src/helpers',
+            '@models': '/src/models',
+            '@constants': '/src/constants',
+            '@layouts': '/src/layouts',
+            '@pages': '/src/pages',
+            '@hooks': '/src/hooks',
+            '@redux': '/src/redux',
+        },
+    },
 });
