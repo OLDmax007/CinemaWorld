e75ea5b4c68683f2f8682b9a02cc52fdc25d1f0d
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 19:07:32 +0300

    Add size , isActive state and tailwind styles

--- src/components/Movies/Genre/Genres.tsx
+++ src/components/Movies/Genre/Genres.tsx
@@ -1,22 +1,27 @@
 import type {FC} from "react";
-import type {GenreType} from "../../../models/GenreType.ts";
+import type {GenreType} from "@models/GenreType.ts";
 import GenreBadge from "./GenreBadge.tsx";
+import {useParams} from "react-router-dom";
+
 
 export type GenresProps = {
     genres: GenreType[]
     variant: 'default' | 'filter'
+    size: 'small' | 'medium'
 }
 
-const Genres:FC<GenresProps> = ({genres,variant}) => {
-    const allGenre = { id: 0, name: 'All Movies' };
-    const genresWithAll = [allGenre,...genres]
+const Genres: FC<GenresProps> = ({genres, variant, size}) => {
+    const {genreName} = useParams<{ genreName: string }>()
+    if (!genres.length || !genres) return <span>No genres</span>
 
     return (
-        <div>
-            {genresWithAll.map((genre) =>
-                <GenreBadge key={genre.id} variant={variant} genre={genre}/>
+        <section>
+            <ul className={'grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] justify-center items-center gap-2 w-full my-6!'}>{genres.map((genre) =>
+                <GenreBadge key={genre.id} variant={variant} size={size} genre={genre}
+                            isActive={genre.slugName === genreName}/>
             )}
-        </div>
+            </ul>
+        </section>
     );
 };
 
