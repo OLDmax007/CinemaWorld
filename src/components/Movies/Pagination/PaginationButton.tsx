fbef5a63c304742137b5181162682a4a89992863
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 17:41:04 +0300

    Create and implement PaginationButton component

---
+++ src/components/Movies/Pagination/PaginationButton.tsx
@@ -0,0 +1,28 @@
+import type {FC, ReactNode} from "react";
+import styles from './pagination.module.css'
+
+type PaginationButtonProps = {
+    currentPage: number;
+    targetPage: number;
+    totalPages: number;
+    onPageChange: (page: number) => void;
+    children: ReactNode;
+};
+
+const PaginationButton: FC<PaginationButtonProps> = ({currentPage, targetPage, totalPages, onPageChange, children}) => {
+
+    const isDisabled = targetPage < 1 || targetPage > totalPages || targetPage === currentPage;
+    if (totalPages === 1 && targetPage === 1) return null;
+
+    return (
+        <button
+            className={styles.pageButton}
+            disabled={isDisabled}
+            onClick={() => onPageChange(currentPage)}
+        >
+            {children}
+        </button>
+    )
+};
+
+export default PaginationButton
