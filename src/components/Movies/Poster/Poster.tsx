ea9ef95104776583a8086540565ae2937424909b
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 17:35:28 +0300

    1. Add loading state with spinner, change tailwind styles and update imports 2. Remove styles in poster module css

--- src/components/Movies/Poster/Poster.tsx
+++ src/components/Movies/Poster/Poster.tsx
@@ -1,20 +1,27 @@
-
-import type {FC} from "react";
-import noImage from '../../../assets/images/no-image.png';
-import {urls} from "../../../constants/urls.ts";
+import {type FC, useState} from "react";
+import noImage from '@assets/images/no-image.png';
+import {urls} from "@constants/urls.ts";
 import styles from './poster.module.css'
-
+import {Atom} from "react-loading-indicators";
 
 type PosterProps = {
-    posterPath: string
-
+    posterPath: string | null
 }
 
 const Poster: FC<PosterProps> = ({posterPath}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const Loading = <div className={'h-full flex justify-center items-center'}>
+        <Atom color="#dfdccc" size="large" text="" textColor="#ff00f8"/>
+    </div>
+
     return (
-        <div className={styles.posterWrapper}>
+        <div
+            className={`${styles.posterWrapper} w-[250px] h-[375px] max-[300px]:w-[150px] max-[300px]:h-[200px] 
+            rounded-[12px] overflow-hidden border-[3px] border-white/12`}>
+            {isLoading && Loading}
             <img
-                className={styles.posterImage}
+                onLoad={() => setIsLoading(false)}
+                className={'w-full h-full object-cover'}
                 src={posterPath ? urls.movies.posterUrl + posterPath : noImage}
                 alt="Poster"/>
         </div>
