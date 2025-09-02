6d22e9f68d1d20cca2353bddcbd26bd8312197fa
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 17:41:16 +0300

    Create and implement PaginationNumButtons component

---
+++ src/components/Movies/Pagination/PageNumButtons.tsx
@@ -0,0 +1,32 @@
+import type {FC} from "react";
+import styles from './pagination.module.css'
+
+type PageNumButtonsProps = {
+    currentPage: number;
+    totalPages: number;
+    onPageChange: (page: number) => void;
+};
+
+const PageNumButtons: FC<PageNumButtonsProps> = ({currentPage, totalPages, onPageChange}) => {
+    const maxButtons = 4;
+    const start = Math.max(2, Math.min(currentPage - Math.floor(maxButtons / 2), totalPages - maxButtons));
+
+    return (
+        <>
+            {Array.from({length: Math.min(maxButtons, totalPages - 2)}, (_, i) => {
+                const page = start + i;
+                return (
+                    <button
+                        key={page}
+                        className={page === currentPage ? styles.currentPage : styles.pageButton}
+                        onClick={() => onPageChange(page)}
+                    >
+                        {page}
+                    </button>
+                );
+            })}
+        </>
+    );
+};
+
+export default PageNumButtons;
