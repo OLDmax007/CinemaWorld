48aedb1464b6f5f12f0b5d87c1aa769f614c81af
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 03:24:45 +0300

    Change styles, add loading wrapper and genre slug check

--- src/layouts/FiltersLayout.tsx
+++ src/layouts/FiltersLayout.tsx
@@ -1,39 +1,38 @@
-import {useAppThunkData} from "../hooks/useAppThunkData.ts";
-import {movieThunks} from "../redux/slices/movieSlice/movieThunks.ts";
-import {useAppPageParam} from "../hooks/useAppPageParam.ts";
-import Genres from "../components/Movies/Genre/Genres.tsx";
-import SearchForm from "../components/UI/SearchForm/SearchForm.tsx";
+import {useAppThunkData} from "@hooks/useAppThunkData.ts";
+import {movieThunks} from "@redux/slices/movieSlice/movieThunks.ts";
+import Genres from "@components/Movies/Genre/Genres.tsx";
+import SearchForm from "@components/Movies/SearchForm/SearchForm.tsx";
 import {Outlet, useParams} from "react-router-dom";
-import EmptyMovies from "../components/UI/EmptyMovies.tsx";
+import EmptyMovies from "@components/UI/EmptyMovies.tsx";
+import {useAppSelector} from "@hooks/useAppSelector.ts";
+import LoadingWrapper from "@components/UI/LoadingWrapper.tsx";
+import type {GenreType} from "@models/GenreType.ts";
 
 const FiltersLayout = () => {
     const genres = useAppThunkData(movieThunks.loadGenres, (state) => state.genres)
-    const page = useAppPageParam()
+    const isLoadingGenres = useAppSelector(state => state.loadingStates.isLoadingGenres)
     const {genreName, genreId} = useParams<{ genreName: string, genreId: string }>()
+
+    if (isLoadingGenres || !genres) return <LoadingWrapper/>;
+
     if (
         genres &&
         genreId &&
         genreName &&
-        !genres.some((g) => g.id === Number(genreId) && g.name === genreName && g.id !== 0)
+        !genres.some((g: GenreType) => g.id === Number(genreId) && g.slugName === genreName)
     ) {
         return <EmptyMovies message="Movies not found"/>;
     }
 
-
     return (
         <>
-            {
-                genres &&
-                <>
-                    <section className={'movies-filters-section'}>
-                        <Genres genres={genres} variant={'default'} size={'medium'}/>
-                        <SearchForm page={page}/>
-                    </section>
-                    <Outlet/>
-                </>
-
-            }
+            <section className={'grid grid-cols-[5fr_3fr] w-full !px-[50px] max-[800px]:grid-cols-1'}>
+                <Genres genres={genres} variant={'default'} size={'medium'}/>
+                <SearchForm/>
+            </section>
+            <Outlet/>
         </>
+
     );
 };
 
