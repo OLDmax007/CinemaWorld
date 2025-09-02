05d02d3d3baa945271e127f60a31bf79946bc113
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 17:44:23 +0300

    Create and implement MovieListSection component

---
+++ src/components/Movies/MoviesList/MoviesListSection.tsx
@@ -0,0 +1,32 @@
+import Pagination from "@components/Movies/Pagination/Pagination.tsx";
+import type {MovieType} from "@models/MovieType.ts";
+import type {FC} from "react";
+import type {TMDBResQueryType} from "@models/TMDBResponseType.ts";
+import {useParams} from "react-router-dom";
+import {useAppQueryParams} from "@hooks/useAppQueryParams.ts";
+import MoviesListItems from "./MoviesListItems.tsx";
+
+export type MoviesListSectionProps = {
+    movies: MovieType[],
+    queryMeta: TMDBResQueryType | null
+}
+
+const MoviesListSection: FC<MoviesListSectionProps> = ({movies, queryMeta}) => {
+    const {genreName} = useParams<{ genreName: string }>()
+    const {query} = useAppQueryParams()
+    return (
+        <section className={`flex flex-col justify-center items-center min-h-screen !px-12`}>
+            <h2>
+                {genreName
+                    ? `Movies by genre: ${genreName}`
+                    : query
+                        ? `Search results for "${query}"`
+                        : 'All movies'}
+            </h2>
+            <MoviesListItems movies={movies}/>
+            <Pagination queryMeta={queryMeta}/>
+        </section>
+    );
+};
+
+export default MoviesListSection;
