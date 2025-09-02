d21a7d72e63f1dd0d9c9461cf37fb47d3e070f0b
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-09-02 18:48:45 +0300

    Move logic to useAppPagination hook and split buttons into components

--- src/components/Movies/MoviesList/MoviesList.tsx
+++ src/components/Movies/MoviesList/MoviesList.tsx
@@ -1,44 +1,33 @@
-import {useAppSelector} from "../../../hooks/useAppSelector.ts";
-import Pagination from "../../UI/Pagination/Pagination.tsx";
-import type {MovieType} from "../../../models/MovieType.ts";
+import {useAppSelector} from "@hooks/useAppSelector.ts";
+import type {MovieType} from "@models/MovieType.ts";
 import {useCallback} from "react";
-import MoviesListCard from "./MoviesListCard.tsx";
-import {useAppPageParam} from "../../../hooks/useAppPageParam.ts";
-import {movieThunks} from "../../../redux/slices/movieSlice/movieThunks.ts";
-import {useAppThunkData} from "../../../hooks/useAppThunkData.ts";
-import styles from './movies.module.css'
-import RenderStatus from "../../UI/RenderStatus.tsx";
+import {useAppQueryParams} from "@hooks/useAppQueryParams.ts";
+import {movieThunks} from "@redux/slices/movieSlice/movieThunks.ts";
+import {useAppThunkData} from "@hooks/useAppThunkData.ts";
+import LoadingWrapper from "@components/UI/LoadingWrapper.tsx";
 import {useParams} from "react-router-dom";
-import EmptyMovies from "../../UI/EmptyMovies.tsx";
-
+import EmptyMovies from "@components/UI/EmptyMovies.tsx";
+import MoviesListSection from "./MoviesListSection.tsx";
 
 const MoviesList = () => {
-    const {queryMeta, isLoadingMovies} = useAppSelector(state => state)
+    const queryMeta = useAppSelector(state => state.queryMeta);
+    const isLoadingMovies = useAppSelector(state => state.loadingStates.isLoadingMovies);
     const {genreId} = useParams<{ genreId: string }>()
-    const page = useAppPageParam()
+    const {page} = useAppQueryParams()
     const loadMoviesCallback = useCallback(() => {
         return movieThunks.loadMovies({
             page,
-            ...(genreId ? {with_genres: genreId.toString()} : {})
+            ...((genreId && +genreId !== 0) ? {with_genres: genreId.toString()} : {})
         });
     }, [page, genreId]);
 
     const movies = useAppThunkData<MovieType[]>(loadMoviesCallback, (state) => state.movies)
 
+    if (isLoadingMovies) return <LoadingWrapper/>
+
     if (!movies || !movies.length) return <EmptyMovies message="Movies not found"/>;
 
-    return (
-        <section className={styles.moviesSection}>
-            <RenderStatus isLoading={isLoadingMovies}>
-                <ul className={styles.moviesList}>
-                    {movies.map((movie) => (
-                        <MoviesListCard key={movie.id} movie={movie}/>
-                    ))}
-                </ul>
-                <Pagination queryMeta={queryMeta}/>
-            </RenderStatus>
-        </section>
-    );
+    return <MoviesListSection movies={movies} queryMeta={queryMeta}/>
 };
 
 export default MoviesList;
